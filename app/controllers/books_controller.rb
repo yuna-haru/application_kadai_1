2899b174c714290a0428c648aa37ac77090db114
EC2 Default User <ec2-user@ip-172-31-35-197.ec2.internal>
2022-03-23 12:50:08 +0000

    validates

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,13 @@
 class BooksController < ApplicationController
  
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to '/books/:id'
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def index
